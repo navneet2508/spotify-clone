b3dbbaa9c1adfd9f9aa839ebe07e238deb0b3a74
navneet2508 <navneety250801@gmail.com>
2022-05-29 18:58:10 +0530

    .

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,7 +4,7 @@ import styled from "styled-components";
 export default function Login() {
   const handleClick = async () => {
     const client_id = "adafa8748e3343ddbdef8ac7d222fb69";
-    const redirect_uri = "http://localhost:3000/";
+    const redirect_uri = "https://illustrious-gingersnap-de8787.netlify.app/";
     const api_uri = "https://accounts.spotify.com/authorize";
     const scope = [
       "user-read-private",
